277cd566e06ecdd8422b6441aa28b56846a1dd2b
Alexandros Katechis <akatechis@gmail.com>
2025-08-30 20:47:00 -0400

    chore: refactor seeds, and include them in the test

--- test/heads_up_web/controllers/page_controller_test.exs
+++ test/heads_up_web/controllers/page_controller_test.exs
@@ -1,12 +1,17 @@
 defmodule HeadsUpWeb.PageControllerTest do
   use HeadsUpWeb.ConnCase
+  import Phoenix.LiveViewTest
 
-  @tag :skip
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    resp = html_response(conn, 200)
-    assert resp =~ "Lost Dog"
-    assert resp =~ "Flat Tire"
-    assert resp =~ "Bear In The Trash"
+  describe "Incidents List" do
+    test "GET /", %{conn: conn} do
+      HeadsUp.SeedData.seed_incidents()
+
+      {:ok, _view, html} = live(conn, ~p"/")
+
+      assert html =~ "Lost Dog"
+      assert html =~ "Flat Tire"
+      assert html =~ "Bear In The Trash"
+    end
+
   end
 end
