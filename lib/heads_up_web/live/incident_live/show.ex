46c48d1f4f88095809b6c7593d37e61f80ff0448
Alexandros Katechis <akatechis@gmail.com>
2025-09-04 22:15:30 -0400

    Load urgent incidents asynchronously

--- lib/heads_up_web/live/incident_live/show.ex
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -1,8 +1,8 @@
 defmodule HeadsUpWeb.IncidentLive.Show do
   use HeadsUpWeb, :live_view
 
-  alias HeadsUp.Incidents
   import HeadsUpWeb.CustomComponents
+  alias HeadsUp.Incidents
 
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -10,11 +10,14 @@ defmodule HeadsUpWeb.IncidentLive.Show do
 
   def handle_params(%{"id" => id}, _uri, socket) do
     incident = Incidents.get_incident(id)
+
     socket =
       socket
       |> assign(:page_title, "#{incident.name} Incident Details")
       |> assign(:incident, incident)
-      |> assign(:urgent_incidents , Incidents.urgent_incidents())
+      |> assign_async(:urgent_incidents, fn ->
+        {:ok, %{urgent_incidents: Incidents.urgent_incidents()}}
+      end)
 
     {:noreply, socket}
   end
@@ -28,17 +31,21 @@ defmodule HeadsUpWeb.IncidentLive.Show do
           <.badge text={@incident.status} />
           <header>
             <h2>{@incident.name}</h2>
+
             <div class="priority">
               {@incident.priority}
             </div>
           </header>
+
           <div class="description">
             {@incident.description}
           </div>
         </section>
       </div>
+
       <div class="activity">
         <div class="left"></div>
+
         <div class="right">
           <.urgent_incidents incidents={@urgent_incidents} />
         </div>
@@ -51,20 +58,35 @@ defmodule HeadsUpWeb.IncidentLive.Show do
     ~H"""
     <section>
       <h4>Urgent Incidents</h4>
-      <ul>
-        <.incident_link :for={incident <- @incidents} incident={incident} />
-      </ul>
+
+      <.async_result :let={results} assign={@incidents}>
+        <:loading>
+          <div class="loading">
+            <div class="spinner" />
+          </div>
+        </:loading>
+
+        <:failed :let={{:exit, reason}}>
+          <div class="failed">
+            {reason}
+          </div>
+        </:failed>
+
+        <ul>
+          <.incident_list_item :for={incident <- results} incident={incident} />
+        </ul>
+      </.async_result>
     </section>
     """
   end
 
   attr :incident, HeadsUp.Incidents.Incident, required: true
-  def incident_link(assigns) do
+
+  def incident_list_item(assigns) do
     ~H"""
     <li class="my-2">
-      <.link navigate={~p"/incidents/#{@incident}"} class="flex items-center gap-2 p-2 hover:bg-gray-200">
-        <img src={@incident.image_path} class="inline-block w-1/4" />
-        {@incident.name}
+      <.link patch={~p"/incidents/#{@incident}"} class="flex items-center gap-2 p-2 hover:bg-gray-100">
+        <img src={@incident.image_path} class="inline-block w-1/4" /> {@incident.name}
       </.link>
     </li>
     """
